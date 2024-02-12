b8bcfbe8ca663a2accf662dfec2443e26bd6f3d9
Ranishlakshan <ranishkalakshangsc@gmail.com>
2024-02-11 22:22:34 -0500

    init project

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+
+
+const DashboardPage = () => {
+  return (
+    <div>
+        Dashboard page (protected)
+        
+    </div>
+  );
+}
+
+export default DashboardPage;
